6687b39cc9c9a981ab94b6e23fea4e269a494b36
dylanmcauliffe <50005666+dylanmcauliffe@users.noreply.github.com>
2019-05-01 13:22:38 -0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# stop on error
+set -e
+
+DATA_DIR="/data"
+IPv6=1
+INT=eth0
+
+#copy default config files if not exist in data directory
+[ ! -f $DATA_DIR/dhcpd.conf ] && cp -v /etc/dhcp/dhcpd.conf $DATA_DIR
+[ ! -f $DATA_DIR/dhcpd6.conf ] && cp -v /etc/dhcp/dhcpd6.conf $DATA_DIR
+
+#create default lease files if not exist in data directory
+[ ! -f $DATA_DIR/dhcpd.leases ] && touch $DATA_DIR/dhcpd.leases
+[ ! -f $DATA_DIR/dhcpd6.leases ] && touch $DATA_DIR/dhcpd6.leases
+
+if [ ! -z $IPv6 ]
+then
+	dhcpd -6 -cf $DATA_DIR/dhcpd6.conf -lf $DATA_DIR/dhcpd6.leases -pf $DATA_DIR/dhcpd6.pid -p 667 $INT
+else
+	echo "IPv6 Disabled"
+fi
+
+dhcpd -4 -cf $DATA_DIR/dhcpd.conf -lf $DATA_DIR/dhcpd.leases -pf $DATA_DIR/dhcpd.pid -p 667 $INT
+
+tail -f /dev/null
