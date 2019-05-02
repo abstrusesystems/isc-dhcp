dd4217bcd2073507bf3110f906184c394afd37b7
dylanmcauliffe <50005666+dylanmcauliffe@users.noreply.github.com>
2019-05-02 06:13:12 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,14 +13,14 @@ set -e
 
 if [ ! -z $IPv6 ]
 then
-	dhcpd -6 -cf $DATA_DIR/dhcpd6.conf -lf $DATA_DIR/dhcpd6.leases -pf $DATA_DIR/dhcpd6.pid -p $DHCP_PORT $INT
+	dhcpd -6 -cf $DATA_DIR/dhcpd6.conf -lf $DATA_DIR/dhcpd6.leases -pf $DATA_DIR/dhcpd6.pid -p $DHCP_PORTv6 $INT
 else
 	echo "IPv6 Disabled"
 fi
 
 if [ ! -z $IPv4 ]
 then
-	dhcpd -4 -cf $DATA_DIR/dhcpd.conf -lf $DATA_DIR/dhcpd.leases -pf $DATA_DIR/dhcpd.pid -p $DHCP_PORT $INT
+	dhcpd -4 -cf $DATA_DIR/dhcpd.conf -lf $DATA_DIR/dhcpd.leases -pf $DATA_DIR/dhcpd.pid -p $DHCP_PORTv4 $INT
 else
 	echo "IPv4 Disabled"
 fi
