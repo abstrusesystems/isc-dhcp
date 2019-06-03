b3758e690b598e4b61389252217ac7cd1fff507f
dylanmcauliffe <50005666+dylanmcauliffe@users.noreply.github.com>
2019-06-03 12:20:54 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,17 @@ init_data() {
 	# create root directory
 	mkdir -p ${DATA}
 	
+	# if not directory /etc then create
+	if [[ ! -d ${DATA}/etc ]];
+	then
+		mv /etc/bind ${DATA}/etc
+	fi
+
+	# delete old location
+	rm -rf /etc/bind
+	
+	# link old location to new directory
+	ln -sf ${DATA}/etc /etc/bind
 }
 
 init_data
