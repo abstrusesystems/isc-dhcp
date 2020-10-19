5fef822515c09298103be2e32c5f074cac4ffd68
dylanmcauliffe <50005666+dylanmcauliffe@users.noreply.github.com>
2020-10-18 18:57:36 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,53 +11,53 @@ init_data() {
 	# if not directory /etc then create
 	if [[ ! -d ${DATA}/etc ]];
 	then
-		mv /etc/dhcp ${DATA}/etc
+		mv /etc/dhcp ${DATA}/etc/dhcp
 	fi
 
 	# delete old location
 	rm -rf /etc/dhcp
 	
 	# link old location to new directory
-	ln -sf ${DATA}/etc /etc/dhcp
+	ln -sf ${DATA}/etc/dhcp /etc/dhcp
 	
 	# ensure files exist
-	touch ${DATA}/etc/dhcpd.conf
-	touch ${DATA}/etc/dhcpd6.conf
+	touch ${DATA}/etc/dhcp/dhcpd.conf
+	touch ${DATA}/etc/dhcp/dhcpd6.conf
 
 	# link default file location to new directory
-	ln -sf ${DATA}/etc/dhcpd.conf /etc/dhcpd.conf
-	ln -sf ${DATA}/etc/dhcpd6.conf /etc/dhcpd6.conf
+	ln -sf ${DATA}/etc/dhcpd.conf /etc/dhcp/dhcpd.conf
+	ln -sf ${DATA}/etc/dhcpd6.conf /etc/dhcp/dhcpd6.conf
 
 
 	# if not directory /var then create
 	if [[ ! -d ${DATA}/var ]];
 	then
-		mkdir -p ${DATA}/var/
+		mkdir -p ${DATA}/var/dhcp/
 	fi
 	
 	# delete old location
 	rm -rf /var/lib/dhcp
 	
 	# link old location to new directory
-	ln -sf ${DATA}/var /var/lib/dhcp
-	ln -sf ${DATA}/var /var/db
+	ln -sf ${DATA}/var/lib/dhcp /var/lib/dhcp
+	ln -sf ${DATA}/var/run/dhcp /var/run/dhcp
 
 	# ensure files exist
-	touch ${DATA}/var/dhcpd.leases
-	touch ${DATA}/var/dhcpd6.leases
+	touch ${DATA}/var/dhcp/dhcpd.leases
+	touch ${DATA}/var/dhcp/dhcpd6.leases
 }
 
 init_data
 
 #check for dhcpd configuration in default location
-if [[ ! -f ${DATA}/etc/dhcpd.conf ]];
+if [[ ! -f ${DATA}/etc/dhcp/dhcpd.conf ]];
 then
-	echo "Please place your dhcpd configuration in ${DATA}/etc/dhcpd.conf"
+	echo "Please place your dhcpd configuration in ${DATA}/etc/dhcp/dhcpd.conf"
 fi
 
-if [[ ! -f ${DATA}/etc/dhcpd6.conf ]];
+if [[ ! -f ${DATA}/etc/dhcp/dhcpd6.conf ]];
 then
-	echo "Please place your dhcpd6 configuration in ${DATA}/etc/dhcpd6.conf"
+	echo "Please place your dhcpd6 configuration in ${DATA}/etc/dhcp/dhcpd6.conf"
 fi
 
 # run CMD
